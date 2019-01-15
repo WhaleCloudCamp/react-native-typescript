236a656c0ba076724d3ba85d0f93c29327bf1fb9
xiaohuoni <448627663@qq.com>
2019-01-15 11:07:21 +0800

    :tada:

--- App.tsx
+++ App.tsx
@@ -7,14 +7,13 @@
  */
 
 import React from 'react';
-import {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Hello } from './components/Hello';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu'
 });
 
 type Props = {};
@@ -22,6 +21,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
+        <Hello name='hello' enthusiasmLevel={2} />
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
@@ -35,16 +35,16 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
+    marginBottom: 5
+  }
 });
